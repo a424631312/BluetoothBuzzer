23d8933a82c012b53005c5dea1e276604736b5b2
haoshuhua <haoshuhua@haoshuhuadeMacBook-Pro.local>
2017-07-18 14:38:23 +0800

    Initial Commit

---
+++ QLAntiLost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QLAntiLost
+//
+//  Created by haoshuhua on 2017/7/18.
+//  Copyright © 2017年 QILOO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
